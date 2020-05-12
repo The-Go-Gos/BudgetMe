0206c1481cb7c41f36014d27fd89480b45918e59
Linwei Liu <llwsgr@hotmail.com>
2020-05-12 17:54:03 -0400

    feat: delete products api reducer

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,6 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/receipts', require('./receipts'))
-router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')

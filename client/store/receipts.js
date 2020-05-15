f2988746e53a45206efbc2881b563d0f82638da6
Linwei Liu <llwsgr@hotmail.com>
2020-05-15 15:23:53 -0400

    feat: upload image from fs

--- client/store/receipts.js
+++ client/store/receipts.js
@@ -1,17 +1,10 @@
 import axios from 'axios'
 
 //action type
-// const ADD_RECEIPT = 'ADD_RECEIPT'
 const ANALYZE_RECEIPT = 'ANALYZE_RECEIPT'
 
 //initial state
 const initialState = []
-
-//action creator
-// const addReceipt = receipt => ({
-//   type: ADD_RECEIPT,
-//   receipt
-// })
 
 const analyzeReceipt = receipt => ({
   type: ANALYZE_RECEIPT,
@@ -22,15 +15,14 @@ const analyzeReceipt = receipt => ({
 export const addReceiptThunk = receipt => async dispatch => {
   try {
     await axios.post('/api/receipts', receipt)
-    // dispatch(addReceipt(res.data))
   } catch (error) {
     console.error(error)
   }
 }
 
-export const analyzeReceiptThunk = receipt => async dispatch => {
+export const analyzeReceiptThunk = formData => async dispatch => {
   try {
-    const res = await axios.post('/api/receipts/google', receipt)
+    const res = await axios.post('/api/receipts/google', formData)
     dispatch(analyzeReceipt(res.data))
   } catch (error) {
     console.error(error)
@@ -40,8 +32,6 @@ export const analyzeReceiptThunk = receipt => async dispatch => {
 //reducer
 export default function receiptsReducer(state = initialState, action) {
   switch (action.type) {
-    // case ADD_RECEIPT:
-    //   return [...state, action.receipt]
     case ANALYZE_RECEIPT:
       return action.receipt
     default:

b590b580d954eb664aafaf78ce8977420a4c9457
Linwei Liu <llwsgr@hotmail.com>
2020-05-19 16:16:46 -0400

    ''add isLogin middleware"

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,17 @@
 const router = require('express').Router()
 module.exports = router
 
-/* const isLoggedIn = (req, res, next) => {
+const isLoggedIn = (req, res, next) => {
   const {user} = req
   if (!user) {
     return res.sendStatus(403)
   }
   next()
 }
-*/
 
-router.use('/users', require('./users'))
-router.use('/receipts', require('./receipts'))
-router.use('/finance', require('./finance'))
+router.use('/users', isLoggedIn, require('./users'))
+router.use('/receipts', isLoggedIn, require('./receipts'))
+router.use('/finance', isLoggedIn, require('./finance'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
